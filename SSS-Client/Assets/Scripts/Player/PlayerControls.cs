454f44d261f4d0f9fa25f206c73b4c9d7f636d81
Tonoychan <tonoychan55@gmail.com>
2026-03-16 23:19:42 +0700

    [BUG] - Fixing Issue with Player Movement

--- SSS-Client/Assets/Scripts/Player/PlayerControls.cs
+++ SSS-Client/Assets/Scripts/Player/PlayerControls.cs
@@ -16,9 +16,6 @@ public class PlayerControls : MonoBehaviour
     private float maxTop;
     private float maxBottom;
 
-    [SerializeField] InputActionReference movementAction;
-    [SerializeField] float movementSpeed;
-
     private void OnEnable()
     {
         EnhancedTouchSupport.Enable();
@@ -37,20 +34,16 @@ public class PlayerControls : MonoBehaviour
 
     void Update()
     {
-        #region Using Input System
-/*
-        Vector2 moveDirection = movementAction.action.ReadValue<Vector2>();
-        transform.Translate(moveDirection * movementSpeed * Time.deltaTime);
-*/
-        #endregion
-
         #region Touch Handling
 
         if (Touch.activeTouches.Count > 0)
         {
             Touch touch = Touch.activeTouches[0];
-            Vector3 touchPos = touch.screenPosition;
-            touchPos = mainCamera.ScreenToWorldPoint(touchPos);
+            Vector2 screenPos = touch.screenPosition;
+            if (!float.IsFinite(screenPos.x) || !float.IsFinite(screenPos.y))
+                return;
+            float camZ = mainCamera.transform.position.z;
+            Vector3 touchPos = mainCamera.ScreenToWorldPoint(new Vector3(screenPos.x, screenPos.y, -camZ));
 
             if (Touch.activeTouches[0].phase == TouchPhase.Began)
             {
@@ -66,17 +59,16 @@ public class PlayerControls : MonoBehaviour
             {
                 transform.position = new Vector3(touchPos.x - offset.x, touchPos.y - offset.y, 0);
             }
+            
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, maxLeft, maxRight),
+                Mathf.Clamp(transform.position.y, maxBottom, maxTop), 0f);
         }
-
         #endregion
-
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, maxLeft, maxRight),
-            Mathf.Clamp(transform.position.y, maxBottom, maxTop), 0f);
     }
 
     private IEnumerator SetBoundary()
     {
-        yield return new WaitForEndOfFrame();
+        yield return new WaitForSeconds(0.2f);
 
         maxLeft = mainCamera.ViewportToWorldPoint(new Vector2(0.15f, 0f)).x;
         maxRight = mainCamera.ViewportToWorldPoint(new Vector2(0.85f, 0f)).x;
